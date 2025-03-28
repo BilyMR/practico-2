f9c85b6b04f97a9621691e2313d6c3cc74486de2
Eduardo Miranda <bilymirandarivero@gmail.com>
2025-03-27 21:53:24 -0400

    Create ex3.cpp
    ejercicio 3 completado

---
+++ ex3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int width, height;
+    std::cout<<"Enter rectangle width and height"<<std::endl;
+    std::cin>>width>>height;
+    std::cout<<"Rectangle area is "<<width * height<<std::endl;
+
+    return 0;
+}
