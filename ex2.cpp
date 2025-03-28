41619871dc2afb4256409ca385e378f14665cae8
Eduardo Miranda <bilymirandarivero@gmail.com>
2025-03-27 21:52:44 -0400

    Create ex2.cpp
    ejercicio 2 completado

---
+++ ex2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    double a;
+    double b;
+    
+    std::cin>> a >> b;
+    
+    double c = a / b;
+    std::cout<< c <<std::endl;
+    
+    return 0;
+}
